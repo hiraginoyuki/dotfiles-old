1c6f3a7f4cbb18ccf6d809eb62d25439958d2dad
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-08-01 10:44:24 +0900

    Make install.sh ignore .gitignore

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ DOTPATH=~/.dotfiles
 
 for f in .??*
 do
-	[ "$f" = ".git" ] && continue
+	[ "$f" = ".git"       ] && continue
+	[ "$f" = ".gitignore" ] && continue
 
 	ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
