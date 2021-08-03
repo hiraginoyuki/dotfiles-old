2cef4177cd1f14910483cae256d7cd092c4ee7f1
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-08-03 23:13:14 +0900

    Change order

--- .zshrc.d/99-zplugin.zsh
+++ .zshrc.d/99-zplugin.zsh
@@ -1,13 +1,7 @@
-# plugns
-zplugin light zsh-users/zsh-autosuggestions
-zplugin light zdharma/fast-syntax-highlighting
-zplugin load zdharma/history-search-multi-word
-
-# themes
+zinit snippet OMZL::history.zsh
 zinit snippet OMZL::git.zsh
 zinit snippet OMZL::clipboard.zsh
 zinit snippet OMZL::key-bindings.zsh
-zinit snippet OMZL::history.zsh
 zinit snippet OMZL::prompt_info_functions.zsh
 zinit snippet OMZL::spectrum.zsh
 zinit snippet OMZP::git
@@ -15,4 +9,8 @@ zinit cdclear -q
 setopt promptsubst
 
 zinit snippet OMZT::af-magic.zsh-theme
+
+zplugin light zsh-users/zsh-autosuggestions
+zplugin light zdharma/fast-syntax-highlighting
+zplugin load zdharma/history-search-multi-word
 
