09eb7dfe3b20b53736ffaa695e20b2d74327bfac
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-08-18 21:27:30 +0900

    Support automatic directory creation

--- install.sh
+++ install.sh
@@ -7,8 +7,10 @@ entries="\
   .gitconfig \
 "
 
-for f in $entries
-do
+for f in $entries; do
+  if echo $f | grep -q '/'; then
+    mkdir -p $(echo $f | sed -r 's/\/[^\/]+$//')
+  fi
 	ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
 
