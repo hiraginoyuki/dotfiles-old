01e1a49d6b8a598a5bc409de9d7dee86cbb60ce3
Yuki Hiragino <hiraginoyuki@gmail.com>
2021-08-29 11:44:26 +0900

    FIX multiword shit

--- .zshrc.d/99-zplugin.zsh
+++ .zshrc.d/99-zplugin.zsh
@@ -1,18 +1,23 @@
+zinit cdclear -q
+setopt promptsubst
+
 zinit snippet OMZL::history.zsh
 zinit snippet OMZL::git.zsh
 zinit snippet OMZL::clipboard.zsh
 zinit snippet OMZL::key-bindings.zsh
 zinit snippet OMZL::prompt_info_functions.zsh
 zinit snippet OMZL::spectrum.zsh
+
+zplugin load zsh-users/zsh-autosuggestions
+zplugin load zsh-users/zsh-history-substring-search
+zplugin load zdharma/history-search-multi-word
+zplugin load zdharma/fast-syntax-highlighting
+
 zinit snippet OMZP::git
-zinit snippet OMZP::history-substring-search
+zinit snippet OMZP::history
 zinit snippet OMZP::vi-mode
-zinit cdclear -q
-setopt promptsubst
 
 zinit snippet OMZT::af-magic.zsh-theme
 
-zplugin light zsh-users/zsh-autosuggestions
-zplugin light zdharma/fast-syntax-highlighting
-zplugin load zdharma/history-search-multi-word
+bindkey "^R" history-search-multi-word
 
