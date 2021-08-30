ae4e88debddb771abd7618d95048dcf530612a87
hiragino_yuki <45731869+HiraginoYuki@users.noreply.github.com>
2021-08-30 18:21:09 +0900

    Typo

--- auto-install.sh
+++ auto-install.sh
@@ -2,5 +2,5 @@ git clone https://github.com/HiraginoYuki/dotfiles ~/.dotfiles
 cd ~/.dotfiles
 ./install.sh
 echo "Type 'exec zsh' to reload your shell and have fun!"
-echo "Note: 'source .zshrc' is deprecated. for more reason, visit:"
+echo "Note: 'source .zshrc' is deprecated. for more info, visit:"
 echo "https://blog.mattclemente.com/2020/06/26/oh-my-zsh-slow-to-load.html"
