94235c8fb936a128b91d54c07a37d468c2b4a378
Yuki Hiragino <yuki@shiina.family>
2021-12-02 13:26:38 +0900

    theme

--- .zshrc.d/99-zplugin.zsh
+++ .zshrc.d/99-zplugin.zsh
@@ -1,22 +1,23 @@
 zinit cdclear -q
 setopt promptsubst
 
+alias lazy='zinit ice wait silent'
+
 zinit snippet OMZL::history.zsh
 zinit snippet OMZL::git.zsh
-zinit snippet OMZL::clipboard.zsh
+lazy; zinit snippet OMZL::clipboard.zsh
 zinit snippet OMZL::key-bindings.zsh
 zinit snippet OMZL::prompt_info_functions.zsh
 zinit snippet OMZL::spectrum.zsh
 
-zplugin load zsh-users/zsh-autosuggestions
-zplugin load zsh-users/zsh-history-substring-search
-zplugin load zdharma-continuum/history-search-multi-word
-zplugin load zdharma-continuum/fast-syntax-highlighting
+lazy; zinit load zsh-users/zsh-autosuggestions
+lazy; zinit load zsh-users/zsh-history-substring-search
+lazy; zinit load zdharma-continuum/history-search-multi-word
+zinit load zdharma-continuum/fast-syntax-highlighting
 
 zinit snippet OMZP::git
 zinit snippet OMZP::history
-
-zinit snippet OMZT::af-magic.zsh-theme
+zinit snippet ~/.dotfiles/af-magic.modified.zsh-theme
 
 bindkey "^R" history-search-multi-word
 
