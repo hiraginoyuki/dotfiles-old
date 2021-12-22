e7ff43e0d9386222112cd552b138e2e8e84636a9
Yuki Hiragino <yuki@shiina.family>
2021-12-23 01:05:07 +0900

    obviously it should be unalias'd

--- .zshrc.d/50-zinit.zsh
+++ .zshrc.d/50-zinit.zsh
@@ -12,23 +12,25 @@ zinit cdclear -q
 setopt promptsubst
 
 
-alias lazy='zinit ice wait silent'
+alias _lazy='zinit ice wait silent'
 
-      zinit snippet OMZL::history.zsh
-      zinit snippet OMZL::git.zsh
-lazy; zinit snippet OMZL::clipboard.zsh
-      zinit snippet OMZL::key-bindings.zsh
-      zinit snippet OMZL::prompt_info_functions.zsh
-      zinit snippet OMZL::spectrum.zsh
+       zinit snippet OMZL::history.zsh
+       zinit snippet OMZL::git.zsh
+_lazy; zinit snippet OMZL::clipboard.zsh
+       zinit snippet OMZL::key-bindings.zsh
+       zinit snippet OMZL::prompt_info_functions.zsh
+       zinit snippet OMZL::spectrum.zsh
 
-lazy; zinit load zsh-users/zsh-autosuggestions
-lazy; zinit load zsh-users/zsh-history-substring-search
-lazy; zinit load zdharma-continuum/history-search-multi-word
-      zinit load zdharma-continuum/fast-syntax-highlighting
+_lazy; zinit load zsh-users/zsh-autosuggestions
+_lazy; zinit load zsh-users/zsh-history-substring-search
+_lazy; zinit load zdharma-continuum/history-search-multi-word
+       zinit load zdharma-continuum/fast-syntax-highlighting
 
-      zinit snippet OMZP::git
-      zinit snippet OMZP::history
-      zinit snippet ~/.dotfiles/af-magic-modified.zsh-theme
+       zinit snippet OMZP::git
+       zinit snippet OMZP::history
+       zinit snippet ~/.dotfiles/af-magic-modified.zsh-theme
+
+unalias _lazy
 
 bindkey "^R" history-search-multi-word
 
