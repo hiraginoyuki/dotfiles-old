cf989f1838b77105a4ac77c966b020e910a08b07
Mogyuchi <git@mogyuchi.com>
2022-06-16 13:08:45 +0900

    update dfu

--- .zshrc.d/99-dotfiles-integration.zsh
+++ .zshrc.d/99-dotfiles-integration.zsh
@@ -1,3 +1,2 @@
 alias cdf="cd ~/.dotfiles"
-alias dfu="zsh -c 'cd ~/.dotfiles && git pull' && zinit update --quiet && exec zsh"
-
+alias dfu="zsh -c 'cd ~/.dotfiles && git stash -a && git switch master && git pull --ff-only && git switch - && git stash pop' && zinit update --quiet && exec zsh"
