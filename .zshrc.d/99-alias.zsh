4a0bcc350ea8d0905b6a48587e3128c6f28b7923
Mogyuchi <git@mogyuchi.com>
2022-06-16 13:40:20 +0900

    alias はシングルクォート

--- .zshrc.d/99-alias.zsh
+++ .zshrc.d/99-alias.zsh
@@ -1,23 +1,23 @@
-alias ":q"=exit
-alias ":e"=vim
-alias reload="exec zsh"
-alias ls="ls --color=auto"
-alias la="ls -lah"
+alias ':q'=exit
+alias ':e'=vim
+alias reload='exec zsh'
+alias ls='ls --color=auto'
+alias la='ls -lah'
 alias ll=la
-alias ld="ls -lah"
-alias nf="neofetch"
+alias ld='ls -lah'
+alias nf='neofetch'
 alias cls=clear
-alias clsls="clear && ls"
-alias cb="xsel --clipboard --input"
-type docker-compose > /dev/null && alias dc=docker-compose || alias dc="docker compose"
-alias downrm="docker-compose down --rmi all --volumes --remove-orphans"
+alias clsls='clear && ls'
+alias cb='xsel --clipboard --input'
+type docker-compose > /dev/null && alias dc=docker-compose || alias dc='docker compose'
+alias downrm='docker-compose down --rmi all --volumes --remove-orphans'
 alias dog=cat
-alias less="less -r"
-alias tree="tree -C"
-alias tl="tree | less"
-alias tal="tree -a |less"
-alias gdc="git diff --compact-summary --diff-filter=d"
-alias grep="grep --color=auto"
+alias less='less -r'
+alias tree='tree -C'
+alias tl='tree | less'
+alias tal='tree -a |less'
+alias gdc='git diff --compact-summary --diff-filter=d'
+alias grep='grep --color=auto'
 
 for i in {1..99}; do
   alias "awk${i}"="awk '{print \$${i}}'"
@@ -80,28 +80,28 @@ elif [[ -f /etc/os-release ]] && grep -q fedora /etc/os-release; then
   alias s="${sudo} dnf search"
 fi
 
-alias t="tmux"
-alias ta="tmux attach"
-alias tk="tmux kill-server"
+alias t='tmux'
+alias ta='tmux attach'
+alias tk='tmux kill-server'
 
-alias vz="vim ~/.zshrc"
-alias ve="vim ~/.zshrc.d/00-env.zsh"
-alias va="vim ~/.zshrc.d/99-alias.zsh"
-alias vb="vim ~/.zshrc.d/99-bind.zsh"
+alias vz='vim ~/.zshrc'
+alias ve='vim ~/.zshrc.d/00-env.zsh'
+alias va='vim ~/.zshrc.d/99-alias.zsh'
+alias vb='vim ~/.zshrc.d/99-bind.zsh'
 
-alias svim="sudoedit"
-alias vi3="vim ~/.config/i3/config"
-alias vx="vim ~/.vnc/xstartup"
-alias vX="vim ~/.Xresources"
-alias vzd="vim ~/.zshrc.d/"
-alias vn="vim ~/.config/nvim/init.vim"
-alias vp="vim ~/.config/nvim/init.d/plugins.vim"
-alias vt="vim ~/.dotfiles/*.zsh-theme && zinit update --quiet ~/.dotfiles/*.zsh-theme > /dev/null; true"
-alias vs="sudo vim /etc/apt/sources.list"
-alias vsd="sudo vim /etc/apt/sources.list.d"
-alias vzp="vim ~/.zshrc.d/99-zplugin.zsh"
-alias xy="xargs -P16 -n1 youtube-dl"
+alias svim='sudoedit'
+alias vi3='vim ~/.config/i3/config'
+alias vx='vim ~/.vnc/xstartup'
+alias vX='vim ~/.Xresources'
+alias vzd='vim ~/.zshrc.d/'
+alias vn='vim ~/.config/nvim/init.vim'
+alias vp='vim ~/.config/nvim/init.d/plugins.vim'
+alias vt='vim ~/.dotfiles/*.zsh-theme && zinit update --quiet ~/.dotfiles/*.zsh-theme > /dev/null; true'
+alias vs='sudo vim /etc/apt/sources.list'
+alias vsd='sudo vim /etc/apt/sources.list.d'
+alias vzp='vim ~/.zshrc.d/99-zplugin.zsh'
+alias xy='xargs -P16 -n1 youtube-dl'
 
-alias gf="git fetch -p --all"
+alias gf='git fetch -p --all'
 
-alias cu="corepack prepare {pnpm@$(npm view pnpm version),yarn@$(curl -s https://api.github.com/repos/yarnpkg/berry/releases/latest | jq -r .name)} --activate"
+alias cu='corepack prepare {pnpm@$(npm view pnpm version),yarn@$(curl -s https://api.github.com/repos/yarnpkg/berry/releases/latest | jq -r .name)} --activate'
